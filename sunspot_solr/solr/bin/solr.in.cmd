c0ff8e396c5d5cd049fb97e9ac19b512e5e17961
Sriram <ksrirambecse@gmail.com>
2023-01-24 18:07:55 +0530

    Update solr.in.cmd

--- sunspot_solr/solr/bin/solr.in.cmd
+++ sunspot_solr/solr/bin/solr.in.cmd
@@ -35,7 +35,6 @@ set GC_TUNE=-XX:NewRatio=3 ^
  -XX:SurvivorRatio=4 ^
  -XX:TargetSurvivorRatio=90 ^
  -XX:MaxTenuringThreshold=8 ^
- -XX:+UseConcMarkSweepGC ^
  -XX:ConcGCThreads=4 -XX:ParallelGCThreads=4 ^
  -XX:+CMSScavengeBeforeRemark ^
  -XX:PretenureSizeThreshold=64m ^
